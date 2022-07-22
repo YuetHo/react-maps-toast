076dfb76c6719050cc964152ad1e8ab5bf3afa0b
Yuet Ho <yho11@fordham.edu>
2022-07-22 16:52:56 -0400

    Added Toast

--- components/places.tsx
+++ components/places.tsx
@@ -1,5 +1,5 @@
 import usePlacesAutocomplete, // autocomplete hook which returns values to use to display and control google places
-{ 
+{
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
@@ -11,6 +11,8 @@ import {
   ComboboxOption,
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import { ToastContainer, toast } from 'react-toastify';  // toast notifications
+import 'react-toastify/dist/ReactToastify.css';
 
 type PlacesProps = {
   setOffice: (position: google.maps.LatLngLiteral) => void;
@@ -26,7 +28,7 @@ export default function Places({ setOffice }: PlacesProps) {
     setValue,
     suggestions: { status, data },  // status of if we actual recieved suggestions, and the actual data
     clearSuggestions,  // whenever user selected a suggestion, we should remove others
-  } = usePlacesAutocomplete();  
+  } = usePlacesAutocomplete();
 
   // async function because you work with promises inside of it
   // handles user clicking on a suggestion in the search
@@ -39,15 +41,22 @@ export default function Places({ setOffice }: PlacesProps) {
     // now to convert address string to coordinates
     // geocode uses the text of the place and return the lat/lng coordinates
     // await it operator used in async functions to wait for a promise
-    const results = await getGeocode({ address: val }); 
+    const results = await getGeocode({ address: val });
     const { lat, lng } = await getLatLng(results[0]); // there is only one element in results, grab the first one
     setOffice({ lat, lng });  // set office state to coordinate, this state as defined in map module will then pan map to location
   };
+
+  // toast message
+  const notify = () => toast("Wow so easy !");
 
   // this will be your search bar, and dropdown suggested places list
   return (
     // combobox needs a onSelect function for when user clicks on any of the suggested options popup
     <Combobox onSelect={handleSelect}>
+
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer />
+
       {/* inside the box is the input */}
       <ComboboxInput
         value={value} // user input value, we need to listen for the value
@@ -61,7 +70,7 @@ export default function Places({ setOffice }: PlacesProps) {
       <ComboboxPopover>
         <ComboboxList>
           { // verify google says status is OK, you got places to work with
-            status === "OK" && 
+            status === "OK" &&
             // if status was true then also map over the data
             // using each place's id and description, render a combobox option in the list
             data.map(({ place_id, description }) => (
